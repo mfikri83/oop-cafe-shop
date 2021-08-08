e18f9d8a937233d8a98bae5d8dd8be054d1a55ac
mfikri83 <muhamadfikri8317@gmail.com>
2021-08-08 20:45:26 +0700

    add variable totalPayment in confirm.php

--- confirm.php
+++ confirm.php
@@ -12,10 +12,12 @@
 <body>
   <div class="order-wrapper">
     <h2>Keranjang</h2>
+    <?php $totalPayment = 0 ?>
     <?php foreach ($menus as $menu): ?>
         <?php 
             $orderCount = $_POST[$menu->getName()];
             $menu->setOrderCount($orderCount);
+            $totalPayment += $menu->getTotalPrice();
         ?>
         <p class="order-amount">
             <?= $menu->getName() ?>
@@ -24,6 +26,7 @@
         </p>
         <p class="order-price">$<?= $menu->getTotalPrice() ?></p>
     <?php endforeach ?>
+    <h3>Harga total: $<?= $totalPayment ?></h3>
   </div>
 </body>
 </html>
