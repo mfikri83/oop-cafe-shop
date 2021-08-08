5afd8e595e98de60f3d8039974de793ed2586820
mfikri83 <muhamadfikri8317@gmail.com>
2021-08-08 20:12:02 +0700

    add form index.php

--- index.php
+++ index.php
@@ -11,17 +11,20 @@
 <body>
   <div class="menu-wrapper container">
     <h1 class="logo">Café Shop</h1>
-    <div class="menu-items">
-      <?php foreach ($menus as $menu): ?>
-        <div class="menu-item">
-            <img src="<?= $menu->getImage() ?>">
-            <h3 class="menu-item-name"><?= $menu->getName() ?></h3>
-            <p class="price">$<?= $menu->getTaxIncludedPrice() ?> (termasuk pajak)</p>
-            <p>Qty: <?= $menu->getOrderCount(); ?></p>
+    <form action="confirm.php" method="post">
+        <div class="menu-items">
+          <?php foreach ($menus as $menu): ?>
+            <div class="menu-item">
+                <img src="<?= $menu->getImage() ?>">
+                <h3 class="menu-item-name"><?= $menu->getName() ?></h3>
+                <p class="price">$<?= $menu->getTaxIncludedPrice() ?> (termasuk pajak)</p>
+                <p>Qty: <?= $menu->getOrderCount(); ?></p>
+                <input type="text" value="0" name="<?= $menu->getName() ?>">
+            </div>
+          <?php endforeach ?>
         </div>
-      <?php endforeach ?>
-      
-    </div>
+        <input type="submit" value="Pesan">
+    </form>
   </div>
 </body>
 </html>
