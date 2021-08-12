f5b16b41ee9bfb817d7d6e12f60c890ed395f106
mfikri83 <muhamadfikri8317@gmail.com>
2021-08-12 23:47:07 +0700

    add file and class User.php

---
+++ classes/User.php
@@ -0,0 +1,19 @@
+<?php 
+class User {
+    private $name;
+    private $gender;
+
+    public function __construct($name, $gender){
+        $this->name = $name;
+        $this->gender = $gender;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getGender() {
+        return $this->gender;
+    }
+}
+?>
